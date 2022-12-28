85da28aa71a77db61010f0e4db61acad7f7985b6
Gustavo Lira <gu.lirar23@gmail.com>
2022-12-28 12:47:07 -0600

    add models to rest request

---
+++ ecommerce/src/main/java/com/training/ecommerce/restmodels/ProductCategoryRestModel.java
@@ -0,0 +1,9 @@
+package com.training.ecommerce.restmodels;
+
+import lombok.Data;
+
+@Data
+public class ProductCategoryRestModel {
+    
+    private String categoryName;
+}
---
+++ ecommerce/src/main/java/com/training/ecommerce/restmodels/ProductRestModel.java
@@ -0,0 +1,26 @@
+package com.training.ecommerce.restmodels;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import com.training.ecommerce.entities.ProductCategory;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductRestModel {
+
+    private String sku;
+    private String name;
+    private String description;
+    private BigDecimal unitPrice;
+    private String imageUrl;
+    private boolean active1;
+    private int unitsInStock;
+    private LocalDate date_created;
+    private Long categoryId;
+}
