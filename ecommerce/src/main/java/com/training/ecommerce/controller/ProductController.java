3d76d8284a323e1c414313af74d25d648d32c178
Gustavo Lira <gu.lirar23@gmail.com>
2022-12-29 10:44:33 -0600

    add cross orgin url to spring  controller

--- ecommerce/src/main/java/com/training/ecommerce/controller/ProductController.java
+++ ecommerce/src/main/java/com/training/ecommerce/controller/ProductController.java
@@ -3,6 +3,7 @@ package com.training.ecommerce.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import com.training.ecommerce.service.ProductService;
 
 @RestController
 @RequestMapping("/api/products")
+@CrossOrigin("http://localhost:4200")
 public class ProductController {
     
     @Autowired
