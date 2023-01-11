ac374d8d0b6c3548a4ecc8027a063100c4294b1f
Gustavo Lira <gu.lirar23@gmail.com>
2023-01-11 13:18:39 -0600

    add routes for products

--- angular-ecommerce-v2/src/app/app-routing.module.ts
+++ angular-ecommerce-v2/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { ProductListByCategoryComponent } from './components/product-list-by-category/product-list-by-category.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", component: ProductListComponent},
+  {path:"category/:id", component: ProductListByCategoryComponent},
+  {path:"**", component: NotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
