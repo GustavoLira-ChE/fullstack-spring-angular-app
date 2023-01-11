ae51026d0cd5adcc4dd99e21c25c9e1fbee58916
Gustavo Lira <gu.lirar23@gmail.com>
2023-01-11 13:10:33 -0600

    create new method for request products by category

--- angular-ecommerce-v2/src/app/services/product.service.ts
+++ angular-ecommerce-v2/src/app/services/product.service.ts
@@ -2,21 +2,31 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, map } from 'rxjs';
 import { Product } from '../models/product.model';
+import { ProductCategory } from '../models/productCategory.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  private baseUrl = "http://localhost:8081/api/products";
+  private baseUrlProducts = "http://localhost:8081/api/products";
+  private baseUrlCategory = "http://localhost:8081/api/category";
 
   constructor(
     private httpClient: HttpClient
   ) {  }
 
   getProductList(): Observable<Product[]> {
-    return this.httpClient.get<Product[]>(this.baseUrl).pipe(
+    return this.httpClient.get<Product[]>(this.baseUrlProducts).pipe(
       map(response => response)
     );
   }
+
+  getCategoryList(): Observable<ProductCategory[]> {
+    return this.httpClient.get<ProductCategory[]>(this.baseUrlCategory).pipe(
+      map(response => response)
+    )
+  }
+
+
 }
