0da8fa2f6f1f7b7bb3a6abd8cf01234e27dacec3
Gustavo Lira <gu.lirar23@gmail.com>
2023-01-12 16:27:42 -0600

    add reactivity to show how many items have been added to shopping cart

--- angular-ecommerce-v2/src/app/components/header/header.component.ts
+++ angular-ecommerce-v2/src/app/components/header/header.component.ts
@@ -1,18 +1,28 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ShoppingCartService } from 'src/app/services/shopping-cart.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
   @Input() title: string = "";
   activeMobileMenu = false;
+  productCounter = 0;
+  totalPurchase = 0;
 
   constructor(
-    private router: Router
+    private router: Router,
+    private shopping: ShoppingCartService
   ){}
+  ngOnInit(): void {
+    this.shopping.cart$.subscribe(products => {
+      this.productCounter = products.length;
+      this.totalPurchase = products.reduce((acum,current) => acum + current.unitPrice, 0);
+    })
+  }
 
   toggleActiveMobileMenu(){
     this.activeMobileMenu = !this.activeMobileMenu;
